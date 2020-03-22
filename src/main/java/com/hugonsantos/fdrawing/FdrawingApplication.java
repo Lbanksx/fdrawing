7e2c30c7ac1b609ac760ffde3d3e5ad6459b2f07
HugoNSantos <hugonsantos03@gmail.com>
2020-03-22 12:48:51 -0300

    Adapted spring boot

---
+++ src/main/java/com/hugonsantos/fdrawing/FdrawingApplication.java
@@ -0,0 +1,13 @@
+package com.hugonsantos.fdrawing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdrawingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FdrawingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hugonsantos/fdrawing/FdrawingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugonsantos.fdrawing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdrawingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
